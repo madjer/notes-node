bca44ea30217aedcd74d77030258658459c4a339
Madjer <madjerbo@gmail.com>
2018-02-13 08:36:20 -0300

    Listing Notes

--- notes.js
+++ notes.js
@@ -1,4 +1,3 @@
-console.log('Starting notes.js');
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -28,7 +27,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log('Getting all notes');
+    return fetchNotes();
 }
 
 var getNote = (title) => {
